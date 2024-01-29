36dd5ad37507d2d4d49e3d37c0e3b0527075a7a8
Paislee <pshouse13@gmail.com>
2024-01-29 16:08:34 -0600

    Delete quoteScript.py
    broken, don't want anymore

--- quoteScript.py
+++
@@ -1,8 +0,0 @@
-def q_generator(): 
-    
-    quotes = ["Attention is vitality. It connects you with others. It makes you eager. Stay eager. ― Susan Sontag", "Tell me, what is it you plan to do with your one wild and precious life? - Mary Oliver", "For small creatures such as we the vastness is bearable only through love. - Carl Sagan", "The assumption that what currently exists must necessarily exist is the acid that corrodes all visionary thinking. - Murray Bookchin"]
-    
-    for x in quotes: 
-        print(x)
-        
-q_generator()
